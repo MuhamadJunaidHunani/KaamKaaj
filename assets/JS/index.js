8d1bf52864d5eb7ae0d3bfee655d1ecc502df310
muhamadjunaidhunani <junaidhunani890@gmail.com>
2024-09-14 03:41:35 +0500

    Add Firestore funtions

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -8,15 +8,45 @@ import {
 import {
   collection,
   addDoc,
+  serverTimestamp,
   onSnapshot,
+  query,
+  where,
   doc,
   updateDoc,
   deleteDoc,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
+const themeSwitch = document.querySelector(".themeSwitch");
+
+function toggleLoader(state) {
+  let isLoading = state;
+  document.querySelector(".LoaderCont").style.display = isLoading
+    ? "flex"
+    : "none";
+}
+themeSwitch.addEventListener("click", () => {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", newTheme);
+  themeSwitch.classList.toggle("activeThemeBtn", newTheme === "dark");
+});
+
+toggleLoader(true);
+
 if (document.getElementById("loginPage")) {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("User is logged in:", user.email);
+      window.location.href = "/index.html";
+    }
+    else {
+      toggleLoader(false);
+    }
+  });
   const loginForm = document.querySelector(".loginForm");
   loginForm.addEventListener("submit", (e) => {
+    toggleLoader(true);
     e.preventDefault();
     const formData = new FormData(e.target);
     const email = formData.get("email");
@@ -25,19 +55,29 @@ if (document.getElementById("loginPage")) {
       .then((userCredential) => {
         const user = userCredential.user;
         console.log(user);
-      window.location.href = "/index.html";
-
+        window.location.href = "/index.html";
       })
       .catch((error) => {
+        toggleLoader(false);
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorMessage);
+        alert(errorMessage);
       });
   });
 }
 else if (document.getElementById("signupPage")) {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("User is logged in:", user.email);
+      window.location.href = "/index.html";
+    }
+    else {
+      toggleLoader(false);
+    }
+  });
   const signupForm = document.querySelector(".signupForm");
   signupForm.addEventListener("submit", (e) => {
+    toggleLoader(true);
     e.preventDefault();
     const formData = new FormData(e.target);
     const username = formData.get("username");
@@ -46,37 +86,107 @@ else if (document.getElementById("signupPage")) {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
-      window.location.href = "/index.html";
-
+        addDocument("Users", {
+          username,
+          email,
+          password,
+          TimeStamp: serverTimestamp(),
+          userId: user.uid,
+        });
       })
       .catch((error) => {
+        toggleLoader(false);
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorMessage);
+        alert(errorMessage);
       });
   });
 }
 else if (document.getElementById("indexPage")) {
   onAuthStateChanged(auth, (user) => {
-    if (user) {
-      console.log("User is logged in:", user.email);
-    }
-    else {
+    if (!user) {
       console.log("User is not logged in, redirecting to login page...");
       window.location.href = "/login.html";
-    }   
+    }
+    else {
+      toggleLoader(false);
+      const todoLoaderCont = document.querySelector(".todoLoaderCont");
+      const TaskInput = document.querySelector(".taskInput");
+      const taskList = document.querySelector(".taskList");
+
+      const toggleTodoLoder = (TodoState) => {
+        todoLoaderCont.style.display = TodoState ? "flex" : "none";
+      };
+      const addDocument = async (collectionName, data) => {
+        try {
+          const docRef = await addDoc(collection(db, collectionName), data);
+          console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+          toggleTodoLoder(false);
+          console.error("Error adding document: ", e);
+        }
+      };
+      const fetchDocumentsRealTime = (collectionName, callback) => {
+        try {
+          toggleTodoLoder(true)
+
+          const q = query(
+            collection(db, collectionName),
+            where("userTaskId", "==", user.uid)
+          );
+          const unsubscribe = onSnapshot(q, (snapshot) => {
+            const docs = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            toggleTodoLoder(false)
+            callback(docs);
+          });
+          return unsubscribe;
+        } catch (e) {
+          console.error("Error fetching documents: ", e);
+        }
+      };
+      fetchDocumentsRealTime("Tasks", (docs) => {
+        taskList.innerHTML = "";
+        if (docs.length > 0) {
+          docs.map((item , index)=>{
+            const TaskItem = document.createElement("li");
+            TaskItem.classList.add("taskItem");
+            TaskItem.innerHTML =
+            `<input type='checkbox' id='task1'><label for='task1'>${item.taskValue}</label><span class='deleteButton'>✕</span>`;
+            taskList.appendChild(TaskItem);
+          })
+        }else{
+          const TaskItem = document.createElement("li");
+            TaskItem.classList.add("taskItem");
+            TaskItem.innerHTML =
+            "No Data Found";
+            taskList.appendChild(TaskItem);
+        }
+      });
+
+      document.querySelector(".logoutCont").addEventListener("click", () => {
+        signOut(auth)
+          .then(() => {})
+          .catch((error) => {});
+      });
+      document.querySelector(".addButton").addEventListener("click", () => {
+        const taskValue = TaskInput.value;
+        if (taskValue) {
+          TaskInput.value = "";
+          toggleTodoLoder(true);
+          addDocument("Tasks", {
+            taskValue,
+            isDone: false,
+            TimeStamp: serverTimestamp(),
+            userTaskId: user.uid,
+          });
+        }
+      });
+    }
   });
 }
 else {
   document.write("No Routes Match");
 }
-
-// function toggleTheme() {
-//     const currentTheme = document.documentElement.getAttribute('data-theme');
-//     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-//     document.documentElement.setAttribute('data-theme', newTheme);
-//   }
-
-//   // Example of adding an event listener to a button
-//   document.getElementById('theme-toggle-button').addEventListener('click', toggleTheme);
